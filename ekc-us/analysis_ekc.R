a51d6b562beeaecd4e157aa350c25e446dfd03e2
Emmanuel Kodom <125686488+Emmanuel-Kodom@users.noreply.github.com>
2026-01-22 09:51:27 -0500

    Create analysis_ekc.R for EKC project setup
    Added initial setup for analysis script including libraries.

---
+++ ekc-us/analysis_ekc.R
@@ -0,0 +1,10 @@
+# ----------------------------------
+# Project: Environmental Kuznets Curve (US)
+# Author: Emmanuel Kodom
+# ----------------------------------
+
+# Load libraries
+library(tidyverse)
+library(fixest)
+
+# Placeholder for data cleaning / analysis / figures
